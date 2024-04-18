b7b6b226e4a9b38b344a0b291ac9a3c6e95852e9
VictorMYeste <info@estrategiaonline.es>
2024-04-18 10:55:34 +0200

    Add RoBERTa approach

--- approaches/Tests.md
+++ approaches/Tests.md
@@ -45,3 +45,45 @@ Universalism: tolerance constrained:    0.06
 
 Macro average:                          0.16
 
+# bert-baseline (17/04)
+
+Self-direction: thought attained:       0.00
+Self-direction: thought constrained:    0.00
+Self-direction: action attained:        0.12
+Self-direction: action constrained:     0.00
+Stimulation attained:                   0.26
+Stimulation constrained:                0.00
+Hedonism attained:                      0.24
+Hedonism constrained:                   0.00
+Achievement attained:                   0.34
+Achievement constrained:                0.19
+Power: dominance attained:              0.30
+Power: dominance constrained:           0.04
+Power: resources attained:              0.24
+Power: resources constrained:           0.20
+Face attained:                          0.01
+Face constrained:                       0.02
+Security: personal attained:            0.02
+Security: personal constrained:         0.23
+Security: societal attained:            0.30
+Security: societal constrained:         0.41
+Tradition attained:                     0.43
+Tradition constrained:                  0.00
+Conformity: rules attained:             0.37
+Conformity: rules constrained:          0.27
+Conformity: interpersonal attained:     0.00
+Conformity: interpersonal constrained:  0.00
+Humility attained:                      0.00
+Humility constrained:                   0.00
+Benevolence: caring attained:           0.18
+Benevolence: caring constrained:        0.00
+Benevolence: dependability attained:    0.22
+Benevolence: dependability constrained: 0.00
+Universalism: concern attained:         0.34
+Universalism: concern constrained:      0.12
+Universalism: nature attained:          0.45
+Universalism: nature constrained:       0.34
+Universalism: tolerance attained:       0.00
+Universalism: tolerance constrained:    0.03
+
+Macro average:                          0.15
---
+++ approaches/bert-baseline-optuna-scheduler/train_roberta.py
@@ -0,0 +1,232 @@
+import argparse
+import datasets
+import numpy
+import os
+import pandas
+import sys
+import tempfile
+import torch
+import transformers
+import optuna
+
+
+# GENERIC
+
+values = [ "Self-direction: thought", "Self-direction: action", "Stimulation",  "Hedonism", "Achievement", "Power: dominance", "Power: resources", "Face", "Security: personal", "Security: societal", "Tradition", "Conformity: rules", "Conformity: interpersonal", "Humility", "Benevolence: caring", "Benevolence: dependability", "Universalism: concern", "Universalism: nature", "Universalism: tolerance" ]
+labels = sum([[value + " attained", value + " constrained"] for value in values], [])
+id2label = {idx:label for idx, label in enumerate(labels)}
+label2id = {label:idx for idx, label in enumerate(labels)} 
+
+def load_dataset(directory, tokenizer, load_labels=True):
+    sentences_file_path = os.path.join(directory, "sentences.tsv")
+    labels_file_path = os.path.join(directory, "labels.tsv")
+    
+    data_frame = pandas.read_csv(sentences_file_path, encoding="utf-8", sep="\t", header=0)
+
+    # Fix TypeError: TextEncodeInput must be Union[TextInputSequence, Tuple[InputSequence, InputSequence]]
+    data_frame['Text'] = data_frame['Text'].fillna('')
+
+    encoded_sentences = tokenizer(data_frame["Text"].to_list(), truncation=True)
+
+    if load_labels and os.path.isfile(labels_file_path):
+        labels_frame = pandas.read_csv(labels_file_path, encoding="utf-8", sep="\t", header=0)
+        labels_frame = pandas.merge(data_frame, labels_frame, on=["Text-ID", "Sentence-ID"])
+        labels_matrix = numpy.zeros((labels_frame.shape[0], len(labels)))
+        for idx, label in enumerate(labels):
+            if label in labels_frame.columns:
+                labels_matrix[:, idx] = (labels_frame[label] >= 0.5).astype(int)
+        encoded_sentences["labels"] = labels_matrix.tolist()
+
+    encoded_sentences = datasets.Dataset.from_dict(encoded_sentences)
+    return encoded_sentences, data_frame["Text-ID"].to_list(), data_frame["Sentence-ID"].to_list()
+
+
+# TRAINING
+
+def train_with_optuna(training_dataset, validation_dataset, pretrained_model, tokenizer, model_name=None, n_trials=100):
+    # https://github.com/NielsRogge/Transformers-Tutorials/blob/master/BERT/Fine_tuning_BERT_(and_friends)_for_multi_label_text_classification.ipynb
+    def compute_metrics(eval_prediction):
+        prediction_scores, label_scores = eval_prediction
+        predictions = prediction_scores >= 0.0 # sigmoid
+        labels = label_scores >= 0.5
+
+        f1_scores = {}
+        for i in range(predictions.shape[1]):
+            predicted = predictions[:,i].sum()
+            true = labels[:,i].sum()
+            true_positives = numpy.logical_and(predictions[:,i], labels[:,i]).sum()
+            precision = 0 if predicted == 0 else true_positives / predicted
+            recall = 0 if true == 0 else true_positives / true
+            f1_scores[id2label[i]] = round(0 if precision + recall == 0 else 2 * (precision * recall) / (precision + recall), 2)
+        macro_average_f1_score = round(numpy.mean(list(f1_scores.values())), 2)
+
+        return {'f1-score': f1_scores, 'marco-avg-f1-score': macro_average_f1_score}
+    
+    def objective(trial):
+        # Hiperparámetros a optimizar
+        learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-4)
+        batch_size = trial.suggest_categorical('batch_size', [8, 16, 32])
+        num_train_epochs = trial.suggest_int('num_train_epochs', 3, 5)
+        weight_decay = trial.suggest_float('weight_decay', 0.0, 0.1)
+
+        output_dir = tempfile.TemporaryDirectory()
+
+        # Configuración de los argumentos de entrenamiento
+        args = transformers.TrainingArguments(
+            output_dir=output_dir.name,
+            evaluation_strategy="epoch",
+            hub_model_id=model_name,
+            save_strategy="epoch",
+            learning_rate=learning_rate,
+            per_device_train_batch_size=batch_size,
+            per_device_eval_batch_size=batch_size,
+            num_train_epochs=num_train_epochs,
+            weight_decay=weight_decay,
+            load_best_model_at_end=True,
+            metric_for_best_model='marco-avg-f1-score',
+            logging_dir='./logs',  # Guardar logs para cada trial
+        )
+
+        model = transformers.AutoModelForSequenceClassification.from_pretrained(
+            pretrained_model,
+            problem_type="multi_label_classification",
+            num_labels=len(labels),
+            id2label=id2label,
+            label2id=label2id
+        )
+
+        if torch.cuda.is_available():
+            print("Using cuda")
+            model = model.to('cuda')
+
+        # Crear un objeto Trainer
+        trainer = transformers.Trainer(
+            model=model,
+            args=args,
+            train_dataset=training_dataset,
+            eval_dataset=validation_dataset,
+            compute_metrics=compute_metrics,
+            tokenizer=tokenizer
+        )
+
+        # Configuración del scheduler
+        optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+        num_training_steps = num_train_epochs * (len(training_dataset) // batch_size)
+        scheduler = transformers.get_scheduler(
+            "linear",
+            optimizer=optimizer,
+            num_warmup_steps=0,
+            num_training_steps=num_training_steps
+        )
+        # scheduler = StepLR(optimizer, step_size=10, gamma=0.5)
+        # scheduler = ExponentialLR(optimizer, gamma=0.9)
+        # scheduler = CosineAnnealingLR(optimizer, T_max=50)
+
+        # Reemplazar el optimizer y scheduler del trainer
+        trainer.optimizer = optimizer
+        trainer.lr_scheduler = scheduler
+
+        trainer.train()
+        evaluation = trainer.evaluate()
+
+        # Optuna busca minimizar, entonces devolvemos el negativo del F1 para maximizarlo
+        return -evaluation['eval_marco-avg-f1-score']
+    
+    study = optuna.create_study(direction='minimize')
+    study.optimize(objective, n_trials=n_trials, n_jobs=2)
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print(f"  Value: {-trial.value}")
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print(f"    {key}: {value}")
+
+    return study.best_trial
+
+def train(training_dataset, validation_dataset, pretrained_model, tokenizer, model_name=None, batch_size=8, num_train_epochs=5, learning_rate=2e-5, weight_decay=0.01):
+    # https://github.com/NielsRogge/Transformers-Tutorials/blob/master/BERT/Fine_tuning_BERT_(and_friends)_for_multi_label_text_classification.ipynb
+    def compute_metrics(eval_prediction):
+        prediction_scores, label_scores = eval_prediction
+        predictions = prediction_scores >= 0.0 # sigmoid
+        labels = label_scores >= 0.5
+
+        f1_scores = {}
+        for i in range(predictions.shape[1]):
+            predicted = predictions[:,i].sum()
+            true = labels[:,i].sum()
+            true_positives = numpy.logical_and(predictions[:,i], labels[:,i]).sum()
+            precision = 0 if predicted == 0 else true_positives / predicted
+            recall = 0 if true == 0 else true_positives / true
+            f1_scores[id2label[i]] = round(0 if precision + recall == 0 else 2 * (precision * recall) / (precision + recall), 2)
+        macro_average_f1_score = round(numpy.mean(list(f1_scores.values())), 2)
+
+        return {'f1-score': f1_scores, 'marco-avg-f1-score': macro_average_f1_score}
+
+    output_dir = tempfile.TemporaryDirectory()
+    args = transformers.TrainingArguments(
+        output_dir=output_dir.name,
+        save_strategy="epoch",
+        hub_model_id=model_name,
+        evaluation_strategy="epoch",
+        learning_rate=learning_rate,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        num_train_epochs=num_train_epochs,
+        weight_decay=weight_decay,
+        load_best_model_at_end=True,
+        metric_for_best_model='marco-avg-f1-score'
+    )
+
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(
+        pretrained_model, problem_type="multi_label_classification",
+        num_labels=len(labels), id2label=id2label, label2id=label2id)
+    if torch.cuda.is_available():
+        print("Using cuda")
+        model = model.to('cuda')
+
+    print("TRAINING")
+    print("========")
+    trainer = transformers.Trainer(model, args,
+        train_dataset=training_dataset, eval_dataset=validation_dataset,
+        compute_metrics=compute_metrics, tokenizer=tokenizer)
+    trainer.train()
+
+    print("\n\nVALIDATION")
+    print("==========")
+    evaluation = trainer.evaluate()
+    for label in labels:
+        sys.stdout.write("%-39s %.2f\n" % (label + ":", evaluation["eval_f1-score"][label]))
+    sys.stdout.write("\n%-39s %.2f\n" % ("Macro average:", evaluation["eval_marco-avg-f1-score"]))
+
+    return trainer
+
+# COMMAND LINE INTERFACE
+
+cli = argparse.ArgumentParser(prog="RoBERTa")
+cli.add_argument("-t", "--training-dataset", required=True)
+cli.add_argument("-v", "--validation-dataset")
+cli.add_argument("-m", "--model-name")
+cli.add_argument("-o", "--model-directory")
+args = cli.parse_args()
+
+pretrained_model = "roberta-base"
+tokenizer = transformers.RobertaTokenizer.from_pretrained(pretrained_model)
+
+training_dataset, training_text_ids, training_sentence_ids = load_dataset(args.training_dataset, tokenizer)
+validation_dataset = training_dataset
+if args.validation_dataset != None:
+    validation_dataset, validation_text_ids, validation_sentence_ids = load_dataset(args.validation_dataset, tokenizer)
+# trainer = train(training_dataset, validation_dataset, pretrained_model, tokenizer, model_name = args.model_name)
+trainer = train_with_optuna(training_dataset, validation_dataset, pretrained_model, tokenizer, model_name = args.model_name, n_trials=50)
+if args.model_name != None:
+    print("\n\nUPLOAD to https://huggingface.co/" + args.model_name + " (using HF_TOKEN environment variable)")
+    print("======")
+    #trainer.push_to_hub()
+
+if args.model_directory != None:
+    print("\n\nSAVE to " + args.model_directory)
+    print("======")
+    trainer.save_model(args.model_directory)
+
