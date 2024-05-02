1370548e5df297d29d433a123fb8cd96e5da6861
VictorMYeste <info@estrategiaonline.es>
2024-05-02 08:52:12 +0200

    Add prediction of task 1 and initial work in the cascading models

---
+++ approaches/deberta-cascading-models/deberta_task_1.py
@@ -0,0 +1,67 @@
+import os
+import datasets
+import pandas
+import numpy
+import torch
+import sys
+import transformers
+import tempfile
+
+
+# GENERIC
+
+values = [ "Self-direction: thought", "Self-direction: action", "Stimulation",  "Hedonism", "Achievement", "Power: dominance", "Power: resources", "Face", "Security: personal", "Security: societal", "Tradition", "Conformity: rules", "Conformity: interpersonal", "Humility", "Benevolence: caring", "Benevolence: dependability", "Universalism: concern", "Universalism: nature", "Universalism: tolerance" ]
+#labels = sum([[value + " attained", value + " constrained"] for value in values], [])
+labels = values[:]
+id2label = {idx:label for idx, label in enumerate(labels)}
+label2id = {label:idx for idx, label in enumerate(labels)} 
+
+# SETUP
+
+#model_path = "model" # load from directory
+model_path = "JohannesKiesel/valueeval24-bert-baseline-en" # load from huggingface hub
+
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
+model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path)
+sigmoid = torch.nn.Sigmoid()
+
+# PREDICTION
+
+# https://github.com/NielsRogge/Transformers-Tutorials/blob/master/BERT/Fine_tuning_BERT_(and_friends)_for_multi_label_text_classification.ipynb
+def predict(text):
+    """ Predicts the value probabilities (attained and constrained) for each sentence """
+    # "text" contains all sentences (plain strings) of a single text in order (same Text-ID in the input file)
+    encoded_sentences = tokenizer(text, truncation=True, padding=True, return_tensors="pt")
+    sentences_predictions = sigmoid(model(**encoded_sentences).logits)
+    labels = [{id2label[idx]: prediction for idx, prediction in enumerate(predictions.tolist())} for predictions in sentences_predictions]
+    return labels
+
+# EXECUTION
+
+def label(instances):
+    """ Predicts the label probabilities for each instance and adds them to it """
+    text = [instance["Text"] for instance in instances]
+    return [{
+            "Text-ID": instance["Text-ID"],
+            "Sentence-ID": instance["Sentence-ID"],
+            **labels
+        } for instance, labels in zip(instances, predict(text))]
+
+def writeRun(labeled_instances, output_dir):
+    """ Writes all (labeled) instances to the predictions.tsv in the output directory """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_file = os.path.join(output_dir, "predictions.tsv")
+    pandas.DataFrame.from_dict(labeled_instances).to_csv(output_file, header=True, index=False, sep='\t')
+
+# code not executed by tira-run-inference-server (which directly calls 'predict(text)')
+if "TIRA_INFERENCE_SERVER" not in os.environ:
+    dataset_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    labeled_instances = []
+    input_file = os.path.join(dataset_dir, "sentences.tsv")
+    for text_instances in pandas.read_csv(input_file, sep='\t', header=0, index_col=None).groupby("Text-ID"):
+        # label the instances of each text separately
+        labeled_instances.extend(label(text_instances[1].sort_values("Sentence-ID").to_dict("records")))
+    writeRun(labeled_instances, output_dir)
+
