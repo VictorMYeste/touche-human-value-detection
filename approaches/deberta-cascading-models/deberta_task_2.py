afb1dc88f7791796cc9cb444ba3f2cdac8c45280
VictorMYeste <victormyeste@gmail.com>
2024-05-05 16:28:12 +0200

    Add proccessing final results with task 2

--- approaches/deberta-cascading-models/deberta_task_2.py
+++ approaches/deberta-cascading-models/deberta_task_2.py
@@ -6,13 +6,12 @@ import torch
 import sys
 import transformers
 import tempfile
-
+from tqdm import tqdm
 
 # GENERIC
 
 values = [ "Self-direction: thought", "Self-direction: action", "Stimulation",  "Hedonism", "Achievement", "Power: dominance", "Power: resources", "Face", "Security: personal", "Security: societal", "Tradition", "Conformity: rules", "Conformity: interpersonal", "Humility", "Benevolence: caring", "Benevolence: dependability", "Universalism: concern", "Universalism: nature", "Universalism: tolerance" ]
 labels = sum([[value + " attained", value + " constrained"] for value in values], [])
-labels = values[:]
 id2label = {idx:label for idx, label in enumerate(labels)}
 label2id = {label:idx for idx, label in enumerate(labels)} 
 
@@ -21,21 +20,14 @@ label2id = {label:idx for idx, label in enumerate(labels)}
 model_path_1 = "model_task_1"
 tokenizer_1 = transformers.AutoTokenizer.from_pretrained(model_path_1)
 model_1 = transformers.AutoModelForSequenceClassification.from_pretrained(model_path_1)
-pipeline_1 = transformers.pipeline("text-classification", model=model_1, tokenizer=tokenizer_1, return_all_scores=True)
+pipeline_1 = transformers.pipeline("text-classification", model=model_1, tokenizer=tokenizer_1, top_k=None)
 
 model_path_2 = "model_task_2"
 tokenizer_2 = transformers.AutoTokenizer.from_pretrained(model_path_2)
 model_2 = transformers.AutoModelForSequenceClassification.from_pretrained(model_path_2)
-pipeline2 = transformers.pipeline("text-classification", model=model_2, tokenizer=tokenizer_2)
+pipeline_2 = transformers.pipeline("text-classification", model=model_2, tokenizer=tokenizer_2)
 
 # PREDICTION
-
-def readable_label(label):
-    label_map = {
-        'LABEL_0': 'attained',
-        'LABEL_1': 'constrained'
-    }
-    return label_map.get(label, "Unknown label")
 
 # https://github.com/NielsRogge/Transformers-Tutorials/blob/master/BERT/Fine_tuning_BERT_(and_friends)_for_multi_label_text_classification.ipynb
 def predict(text):
@@ -43,36 +35,37 @@ def predict(text):
     # "text" contains all sentences (plain strings) of a single text in order (same Text-ID in the input file)
 
     # Apply Model 1
-    model_1_results = pipeline_1(text)
+    model_1_results = pipeline_1(text, truncation=True)
 
     final_results = []
 
-    for result_1 in model_1_results:
-        pred_dict = {}
-        value = result_1['label']
+    for result_1 in tqdm(model_1_results, desc="Labeling sentences", unit="sentence"):
+        for sentence in result_1:
+            pred_dict = {}
+            value = sentence['label']
 
-        # Initialize predictions as 0.0
-        pred_dict[value + " attained"] = 0.0
-        pred_dict[value + " constrained"] = 0.0
+            # Initialize predictions as 0.0
+            for label in labels:
+                pred_dict[label] = 0.0
 
-        # If this value is present in the text
-        if result_1['score'] > 0.5:
+            # If this value is present in the text
+            if sentence['score'] > 0.5:
 
-            # Apply Model 2
-            input_for_model_2 = f"{text} {value}"
-            model_2_results = pipeline2(input_for_model_2)
+                # Apply Model 2
+                input_for_model_2 = f"{text} {value}"
+                model_2_results = pipeline_2(input_for_model_2, truncation=True)
 
-            # Update predictions with the Model 2 results
-            for result_2 in model_2_results:
-                result_2_label = readable_label(result_2['label'])
-                if result_2['score'] > 0.6:
-                    pred_dict[value + " " + result_2_label] = 1.0
-                elif result_2['score'] < 0.4:
-                    pred_dict[value + " " + result_2_label] = 0.0
-                else:
-                    pred_dict[value + " " + result_2_label] = 0.5
+                # print(model_2_results)
 
-            final_results.append(pred_dict)
+                # Update predictions with the Model 2 results
+                for result_2 in model_2_results:
+                    if result_2['score'] >= 0.6:
+                        pred_dict[value + " " + result_2['label']] = 1.0
+                    elif result_2['score'] >= 0.4:
+                        pred_dict[value + " attained"] = 0.5
+                        pred_dict[value + " constrained"] = 0.5
+
+                final_results.append(pred_dict)
 
     return final_results
 
@@ -100,7 +93,8 @@ if "TIRA_INFERENCE_SERVER" not in os.environ:
     output_dir = sys.argv[2]
     labeled_instances = []
     input_file = os.path.join(dataset_dir, "sentences.tsv")
-    for text_instances in pandas.read_csv(input_file, sep='\t', header=0, index_col=None).groupby("Text-ID"):
+    texts_df = pandas.read_csv(input_file, sep='\t', header=0, index_col=None).groupby("Text-ID")
+    for text_instances in tqdm(texts_df, desc="Labeling Texts", unit="text"):
         # label the instances of each text separately
         labeled_instances.extend(label(text_instances[1].sort_values("Sentence-ID").to_dict("records")))
     writeRun(labeled_instances, output_dir)
